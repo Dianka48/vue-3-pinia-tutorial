1ef89dea850e007d776d1d4116e53839a8576a66
Dianka48 <dianka.melegova@gmail.com>
2022-05-06 14:46:42 +0200

    Course modules completed: refs, reactive, methods, computed, watch, lifecycle hooks, directives, router, teleport, template refs, props, emits, child components.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,196 @@
 <template>
     <div class="home">
-        <h1>Home</h1>
+        <h2 ref="appTitleRef">{{ appTitle }}</h2>
+        <h3>{{ counterData.title }}:</h3>
+        <div>
+            <button @click="decreaseCounter(2)" class="btn">--</button>
+            <button @click="decreaseCounter(1)" class="btn">-</button>
+            <span class="counter">{{ counterData.count }}</span>
+            <button @click="increaseCounter(1, $event)" class="btn">+</button>
+            <button @click="increaseCounter(2)" class="btn">++</button>
+        </div>
+        <p>This counter is {{ oddOrEven }}</p>
+
+        <div class="edit">
+            <h4>Edit counter title:</h4>
+            <input v-model="counterData.title" type="text" v-autofocus />
+        </div>
     </div>
 </template>
+
+<script setup>
+/*
+  imports
+*/
+
+import {
+    reactive,
+    computed,
+    ref,
+    watch,
+    nextTick,
+    // onBeforeMount,
+    onMounted,
+    // onBeforeUnmount,
+    // onUnmounted,
+    // onActivated,
+    // onDeactivated,
+    // onBeforeUpdate,
+    // onUpdated,
+} from "vue";
+import { vAutofocus } from "@/directives/vAutofocus.js";
+
+// const counter = ref(0),
+//     counterTitle = ref("My Counter");
+
+/*
+  app title
+*/
+
+const appTitle = "My Amazing Counter App";
+
+const appTitleRef = ref(null);
+
+onMounted(() => {
+    console.log(`The app title is ${appTitleRef.value.offsetWidth}px wide`);
+});
+
+/*
+  counter
+*/
+
+const counterData = reactive({
+    count: 0,
+    title: "My Counter",
+});
+
+watch(
+    () => counterData.count,
+    (newCount) => {
+        if (newCount === 20) {
+            alert("Way to go! You made it to 20!!");
+        }
+    }
+);
+
+const oddOrEven = computed(() => {
+    if (counterData.count % 2 === 0) {
+        return "even";
+    }
+    return "odd";
+});
+
+const increaseCounter = async (amount, e) => {
+    counterData.count += amount;
+    await nextTick();
+    console.log("do something when counter has updated in the DOM");
+};
+
+const decreaseCounter = (amount) => {
+    counterData.count -= amount;
+};
+
+onMounted(() => {
+    console.log("Do stuff related to counter");
+});
+
+/*
+  hooks
+*/
+
+// onBeforeMount(() => {
+//     console.log("onBeforeMouont");
+// });
+
+// onMounted(() => {
+//     console.log("onMounted");
+// });
+
+// onBeforeUnmount(() => {
+//     console.log("onBeforeUnmount");
+// });
+
+// onUnmounted(() => {
+//     console.log("onUnmounted");
+// });
+
+// onActivated(() => {
+//     console.log("onActivated");
+// });
+
+// onDeactivated(() => {
+//     console.log("onDeactivated");
+// });
+
+// onBeforeUpdate(() => {
+//     console.log("onBeforeUpdate");
+// });
+
+// onUpdated(() => {
+//     console.log("onUpdated");
+// });
+</script>
+
+<!--
+<script>
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const counter = ref(0);
+
+        const increaseCounter = () => {
+            counter.value++;
+        };
+
+        const decreaseCounter = () => {
+            counter.value--;
+        };
+
+        return { counter, increaseCounter, decreaseCounter };
+    },
+};
+</script> -->
+
+<!--
+<script>
+export default {
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+    methods: {
+        increaseCounter() {
+            this.counter++;
+        },
+        decreaseCounter() {
+            this.counter--;
+        },
+    },
+    directives: {
+        autoFocus: {
+            mounted(el) {
+                el.focus();
+            },
+        },
+    },
+};
+</script> -->
+
+<style>
+.home {
+    text-align: center;
+    padding: 20px;
+}
+
+.btn,
+.counter {
+    font-size: 40px;
+    margin: 10px;
+}
+
+.edit {
+    margin-top: 60px;
+}
+</style>
---
+++ src/views/ModalsView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="modals">
+        <h1>Modals</h1>
+        <button @click="showModal = true">Show modal</button>
+        <Modal
+            @hideModal="showModal = false"
+            v-if="showModal"
+            title="My modal title (via prop)"
+        >
+            <!-- <template #title>My new title</template> -->
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Reiciendis exercitationem corporis deleniti a totam, enim
+                dignissimos doloribus? Nam modi quo facere consequatur,
+                obcaecati recusandae adipisci, quidem distinctio minus, fugit
+                reprehenderit?
+            </p>
+        </Modal>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Modal from "../components/Modal.vue";
+
+// modal
+const showModal = ref(false);
+</script>
